b979ac4cf4b85abca88ae3ba0aab5d5e24feed80
Sylvain Bellemare <sylvain@ascribe.io>
2016-07-24 16:12:57 +0200

    Increase micro version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ docs_require = [
 
 setup(
     name='pytest-rethinkdb',
-    version='0.1.0',
+    version='0.1.1',
     author='Sylvain Bellemare',
     author_email='sylvain@bigchaindb.com',
     maintainer='Sylvain Bellemare',
